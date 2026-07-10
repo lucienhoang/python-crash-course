6491149410cefa1a04ef51716fc7c06b80c50ab5
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-10 13:03:53 +0700

    Added code files for entire book.

--- part01_basics/chapter02_variables/apostrophe.py
+++ part01_basics/chapter02_variables/apostrophe.py
@@ -1,3 +1,4 @@
 message = "One of Python's strengths is its driverse community."
 # message = 'One of Python's strengths is its driverse community.'
+# SyntaxError: unterminated string literal (detected at line 1)
 print(message)
