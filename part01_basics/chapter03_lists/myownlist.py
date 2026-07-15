edf17286be3de114210f15958462abb9a8344c1d
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-15 10:06:16 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter03_lists/myownlist.py
@@ -0,0 +1,5 @@
+my_list = ["pretty house", "small coffee shop", "Honda motorcycle", "my wife smile"]
+
+print("I want a " + my_list[0])
+print("I want to open a " + my_list[1])
+print("I want to make " + my_list[-1])
