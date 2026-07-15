8e69e17ec8aa30680fcde04592c9333a9f985421
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-15 21:21:56 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter03_lists/dinner_guests.py
@@ -0,0 +1,4 @@
+guest_lists = ["Dung Dung", "BongBang", "Thao Dung"]
+print(guest_lists)
+
+print("Thank you so much for coming to my dinner. There are " + str(len(guest_lists)) + " great people in the table tonight. Please welcome!")
---
+++ part01_basics/chapter03_lists/every_function.py
@@ -0,0 +1,48 @@
+lists = []
+
+# add
+lists.append('vi em')
+lists.append("towm")
+lists.append("river")
+
+print(lists)
+
+# add with index
+lists.insert(2, 'lau dai')
+print(lists)
+
+# delete 
+del(lists[0])
+print(lists)
+
+# delete but still remember the item
+lists.pop(0) # pop()
+print(lists)
+
+# removing by value
+lists.remove('lau dai')
+print(lists)
+
+# add again
+lists.append('vi em')
+lists.append("towm")
+lists.append("lau dai")
+
+# sort
+print(" === SORT ===")
+lists.sort()
+print(lists)
+
+print("Here is the sorted list:")
+print(sorted(lists, reverse= True))
+
+lists.sort(reverse= True)
+print(lists)
+
+# reverse
+print("Here is the reverse list:")
+lists.reverse()
+print(lists)
+
+# lenth
+print("Total length of the list: " + str(len(lists)))
