7f28e44d62dc5db3414a7fbd446eb4ad2cf7ea67
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-18 13:47:30 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter04_working_with_lists/threes.py
@@ -0,0 +1,9 @@
+multiples_of_3 = list(range(3, 31, 3))
+
+# multiples_of_3 = []
+# for value in range(3, 31, 3):
+#     multiples_of_3.append(value)
+
+print(multiples_of_3)
+
+# [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
