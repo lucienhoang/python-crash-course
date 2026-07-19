8898b111074d386cf650a2f25b6b5dcb75b73670
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-19 09:15:49 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter05_if_statements/ages.py
@@ -0,0 +1,8 @@
+ages_0 = 18
+ages_1 = 21
+
+if (ages_0 >= 18) and (ages_1 >= 21):
+    print("You are old enough to get married and vote.")
+
+if (ages_0 > 20) or (ages_1 >= 21):
+    print("You are too old bro!")
