ab3ec4f4ed45c85d60535db14543c2eb7cfcb98a
khoa.hdinh <khoa.hdinh@gmail.com>
2026-07-19 10:37:25 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter05_if_statements/amusement_park.py
@@ -0,0 +1,19 @@
+age = 66
+
+# if age < 4:
+#     print("Your cost is $0.")
+# elif age < 18:
+#     print("Your cost is $5.")
+# else:
+#     print("Your cost is $10.")
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+elif age < 65:
+    price = 10
+elif age >= 65:  # else:
+    price = 6
+
+print("Your cost is $" + str(price) + ".")
