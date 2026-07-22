d2a6826caa8d3d9294d31b0dc925dfd7912a3c03
lucienhoang <nosewater.station@gmail.com>
2026-07-22 11:54:02 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter05_if_statements/favorite_fruits.py
@@ -0,0 +1,10 @@
+favorite_fruits = ["banh beo", "banh xeo", "banh hoi"]
+
+if "banh xeo" in favorite_fruits:
+    print("I reaaly like " + favorite_fruits[1].title() + "!")
+if "banh beo" in favorite_fruits:
+    print("I reaaly like " + favorite_fruits[0].title() + "!")
+if "banh canh" not in favorite_fruits:
+    print("I don't reaaly like banh canh!")
+if "banh hoi" in favorite_fruits:
+    print("I reaaly like " + favorite_fruits[2].title() + "!")
