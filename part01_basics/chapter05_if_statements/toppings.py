158edb8b1bdf68118671357c75e8555e9d8d0736
lucienhoang <nosewater.station@gmail.com>
2026-07-22 13:39:22 +0700

    Added code files for entire book.

--- part01_basics/chapter05_if_statements/toppings.py
+++ part01_basics/chapter05_if_statements/toppings.py
@@ -3,15 +3,26 @@ requested_topping = "mushrooms"
 if requested_topping != "anchoives":
     print("Hold the Anchoives!")
 
-requested_toppings = ["mushrooms", "extra cheese"]
+# requested_toppings = ["mushrooms", "extra cheese", "pepper"]
+requested_toppings = []
 
-if "mushrooms" in requested_toppings:
-    print("Adding mushrooms.")
-if "pepper" in requested_toppings:
-    print("Adding pepper.")
-if "extra cheese" in requested_toppings:
-    print("Adding extra cheese.")
 
-print("\nFinished making your pizza!")
+# if "mushrooms" in requested_toppings:
+#     print("Adding mushrooms.")
+# if "pepper" in requested_toppings:
+#     print("Adding pepper.")
+# if "extra cheese" in requested_toppings:
+#     print("Adding extra cheese.")
+
+if requested_toppings:  # return true if requested_toppings is not null
+    for requested_topping in requested_toppings:
+        if requested_topping == "mushrooms":
+            print("Sorry, We are out of mushrooms right now!")
+        else:
+            print("Adding " + requested_topping.title() + ".")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
+
 
 # test
