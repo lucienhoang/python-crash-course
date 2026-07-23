eea29499cbef9f631c23934a09950515b8298ad8
lucienhoang <nosewater.station@gmail.com>
2026-07-23 13:05:25 +0700

    Added code files for entire book.

--- part01_basics/chapter04_working_with_lists/animals.py
+++ part01_basics/chapter04_working_with_lists/animals.py
@@ -1,5 +1,11 @@
-animals = ['cat', 'dog', 'bee']
+animals = ["cat", "dog", "bee"]
 
 for animal in animals:
     print("A " + animal.title() + " would make a great pet!")
 print("\nI really like to play with them!")
+
+# A Cat would make a great pet!
+# A Dog would make a great pet!
+# A Bee would make a great pet!
+
+# I really like to play with them!
