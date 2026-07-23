9e8220c08c6b1f901549886e6a8d64468a528e1b
lucienhoang <nosewater.station@gmail.com>
2026-07-23 12:43:17 +0700

    Added code files for entire book.

--- part01_basics/chapter03_lists/guest_lists_2.py
+++ part01_basics/chapter03_lists/guest_lists_2.py
@@ -1,14 +1,19 @@
 guest_lists = ["Dung Dung", "BongBang", "Thao Dung"]
 print(guest_lists)
+# ['Dung Dung', 'BongBang', 'Thao Dung']
 
 stay_home = guest_lists.pop()
 message = "Sorry " + stay_home.title() + ", I can't invite you"
 print(message)
-    
+# Sorry Thao Dung, I can't invite you
+
 stay_home = guest_lists.pop()
 message = "Sorry " + stay_home.title() + ", I can't invite you"
 print(message)
+# Sorry Bongbang, I can't invite you
 
 del guest_lists[0]
 print("Lists of Guest:")
 print(guest_lists)
+# Lists of Guest:
+# []
