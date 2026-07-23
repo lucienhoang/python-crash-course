054041b1720ae961b74ca6836d9fa3ccdc725f59
lucienhoang <nosewater.station@gmail.com>
2026-07-23 12:39:14 +0700

    Added code files for entire book.

--- part01_basics/chapter03_lists/greetings.py
+++ part01_basics/chapter03_lists/greetings.py
@@ -1,13 +1,16 @@
 names = ["Laurence", "Hai", "Hoang", "Luci"]
 
-laurence_message = "Yo, " + names[0] +  "!"
-hai_message = "What sup, " + names[1] +  "?"
-hoang_message = "You borrowed me 47k, " + names[2] +  ". Where is my money?"
+laurence_message = "Yo, " + names[0] + "!"
+hai_message = "What sup, " + names[1] + "?"
+hoang_message = "You borrowed me 47k, " + names[2] + ". Where is my money?"
 luci_message = "Great life, " + names[3]
-
-
 
 print(laurence_message)
 print(hai_message)
 print(hoang_message)
 print(luci_message)
+
+# Yo, Laurence!
+# What sup, Hai?
+# You borrowed me 47k, Hoang. Where is my money?
+# Great life, Luci
