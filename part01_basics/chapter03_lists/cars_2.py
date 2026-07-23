a19a9b3bfa7f34888c3ca0c7a81561ba7b5bb0fc
lucienhoang <nosewater.station@gmail.com>
2026-07-23 12:24:37 +0700

    Added code files for entire book.

--- part01_basics/chapter03_lists/cars_2.py
+++ part01_basics/chapter03_lists/cars_2.py
@@ -1,7 +1,10 @@
 cars = ["bmw", "audi", "toyota", "subaru"]
 print(cars)
+# ['bmw', 'audi', 'toyota', 'subaru']
 
 cars.reverse()
 print(cars)
+# ['subaru', 'toyota', 'audi', 'bmw']
 
 print("Length of the List: " + str(len(cars)))
+# Length of the List: 4
