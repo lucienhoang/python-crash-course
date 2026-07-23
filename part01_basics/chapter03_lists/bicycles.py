c54185f6ed1e555d08da389696ebcadb4cf09b2d
lucienhoang <nosewater.station@gmail.com>
2026-07-23 12:21:09 +0700

    Added code files for entire book.

--- part01_basics/chapter03_lists/bicycles.py
+++ part01_basics/chapter03_lists/bicycles.py
@@ -1,16 +1,16 @@
 # list example
-bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+bicycles = ["trek", "cannondale", "redline", "specialized"]
 print(bicycles)
 
 # Accessing Elements in a List
-print(bicycles[0]) 
-print(bicycles[0].title()) 
+print(bicycles[0])
+print(bicycles[0].title())
 
-#Index positions
+# Index positions
 print(bicycles[1])
 print(bicycles[3])
-print(bicycles[-1]) # === print(bicycles[3])
+print(bicycles[-1])  # === print(bicycles[3])
 
-# Using  Individual Values form a List
+# Using  Individual Values from a List
 message = "My first bicyles was a " + bicycles[0].title() + "."
 print(message)
