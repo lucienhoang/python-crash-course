1340da3d38a5c06c692326804e49aa994dcd93db
lucienhoang <nosewater.station@gmail.com>
2026-07-23 12:28:19 +0700

    Added code files for entire book.

--- part01_basics/chapter03_lists/every_function.py
+++ part01_basics/chapter03_lists/every_function.py
@@ -1,30 +1,35 @@
 lists = []
 
 # add
-lists.append('vi em')
+lists.append("vi em")
 lists.append("towm")
 lists.append("river")
 
 print(lists)
+# ['vi em', 'towm', 'river']
 
 # add with index
-lists.insert(2, 'lau dai')
+lists.insert(2, "lau dai")
 print(lists)
+# ['vi em', 'towm', 'lau dai', 'river']
 
-# delete 
-del(lists[0])
+# delete
+del lists[0]
 print(lists)
+# ['towm', 'lau dai', 'river']
 
 # delete but still remember the item
-lists.pop(0) # pop()
+lists.pop(0)  # pop()
 print(lists)
+# ['lau dai', 'river']
 
 # removing by value
-lists.remove('lau dai')
+lists.remove("lau dai")
 print(lists)
+# ['river']
 
 # add again
-lists.append('vi em')
+lists.append("vi em")
 lists.append("towm")
 lists.append("lau dai")
 
@@ -32,17 +37,22 @@ lists.append("lau dai")
 print(" === SORT ===")
 lists.sort()
 print(lists)
+# ['lau dai', 'river', 'towm', 'vi em']
 
 print("Here is the sorted list:")
-print(sorted(lists, reverse= True))
+print(sorted(lists, reverse=True))
+# ['vi em', 'towm', 'river', 'lau dai']
 
-lists.sort(reverse= True)
+lists.sort(reverse=True)
 print(lists)
+# ['vi em', 'towm', 'river', 'lau dai']
 
 # reverse
 print("Here is the reverse list:")
 lists.reverse()
 print(lists)
+# ['lau dai', 'river', 'towm', 'vi em']
 
 # length
 print("Total length of the list: " + str(len(lists)))
+# Total length of the list: 4
