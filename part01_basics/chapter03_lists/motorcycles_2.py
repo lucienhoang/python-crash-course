6b01c82acadf09d6724b8b37037519c4902bc9ac
lucienhoang <nosewater.station@gmail.com>
2026-07-23 12:49:53 +0700

    Added code files for entire book.

--- part01_basics/chapter03_lists/motorcycles_2.py
+++ part01_basics/chapter03_lists/motorcycles_2.py
@@ -1,7 +1,8 @@
 motorcycles = []
 
-motorcycles.append('honda')
-motorcycles.append('yamaha')
-motorcycles.append('suzuki')
+motorcycles.append("honda")
+motorcycles.append("yamaha")
+motorcycles.append("suzuki")
 
 print(motorcycles)
+# ['honda', 'yamaha', 'suzuki']
