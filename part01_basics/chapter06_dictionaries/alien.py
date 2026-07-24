18f9ae31984d16c285c0fc674e714c5ca4394d91
lucienhoang <nosewater.station@gmail.com>
2026-07-24 10:01:40 +0700

    Added code files for entire book.

--- part01_basics/chapter06_dictionaries/alien.py
+++ part01_basics/chapter06_dictionaries/alien.py
@@ -5,3 +5,6 @@ print(alien_0["point"])
 
 # green
 # 5
+
+# A dictionary in Python is a collection of key-value pairs. Each key is connected
+# to a value, and you can use a key to access the value associated with that key.
