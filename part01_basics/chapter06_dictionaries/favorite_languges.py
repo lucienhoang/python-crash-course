abab34f3d2f798780bedca2296ed7ce00787fba2
lucienhoang <nosewater.station@gmail.com>
2026-07-25 12:18:52 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter06_dictionaries/favorite_languges.py
@@ -0,0 +1,9 @@
+favorite_languages = {
+    "jen": "python",
+    "khoa": "c",
+    "dung": "java",
+    "Mr. bong bang": "ruby",
+}
+
+print("Jen favorite's language is " + favorite_languages["jen"].title() + ".")
+# Jen favorite's language is Python.
