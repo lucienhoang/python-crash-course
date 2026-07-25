548506a2f697fb0543edbfffdb11e8afbb7ea7d7
lucienhoang <nosewater.station@gmail.com>
2026-07-25 14:25:48 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter06_dictionaries/favorite_numbers.py
@@ -0,0 +1,9 @@
+favorite_numbers = {"khoa": 13, "dung": 4, "laurence": 7}
+
+print("Khoa favorite number is: " + str(favorite_numbers["khoa"]))
+print("Dung favorite number is: " + str(favorite_numbers["dung"]))
+print("Laurence favorite number is: " + str(favorite_numbers["laurence"]))
+
+# Khoa favorite number is: 13
+# Dung favorite number is: 4
+# Laurence favorite number is: 7
