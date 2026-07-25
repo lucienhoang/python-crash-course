bfe1caea1632b05240e6a65b17c725072cc7829f
lucienhoang <nosewater.station@gmail.com>
2026-07-25 14:43:51 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter06_dictionaries/glossary.py
@@ -0,0 +1,13 @@
+glossary = {
+    "list": "collection of items",
+    "range()": "generate a series of numbers",
+    "tuple": "a immutable list",
+}
+
+print("List mean: " + glossary["list"])
+print("Range() mean: " + glossary["range()"])
+print("Tuple mean: " + glossary["tuple"])
+
+# List mean: collection of items
+# Range() mean: generate a series of numbers
+# Tuple mean: a immutable list
