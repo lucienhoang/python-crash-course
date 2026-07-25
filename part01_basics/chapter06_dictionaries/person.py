048aef70bb3326a44df18d59f9b4e991aca8881c
lucienhoang <nosewater.station@gmail.com>
2026-07-25 14:01:51 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter06_dictionaries/person.py
@@ -0,0 +1,6 @@
+person = {"first_name": "Le", "last_name": "Dung", "age": 22, "city": "Ha Noi"}
+
+print(person["first_name"])
+print(person["last_name"])
+print(person["age"])
+print(person["city"])
