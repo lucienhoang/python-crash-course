fbeef60401f93b8cb1599502ef27b9d0e4cf6278
lucienhoang <nosewater.station@gmail.com>
2026-07-27 22:17:40 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter06_dictionaries/pets.py
@@ -0,0 +1,15 @@
+# 1. Choose the right data structure: A list of dictionaries is perfect for managing a collection of items.
+spike = {"name": "spike", "owner": "dung", "kind": "dog"}
+tom = {"name": "tom", "owner": "khoa", "kind": "cat"}
+jerry = {"name": "jerry", "owner": "laurence", "kind": "mouse"}
+
+pets = [spike, tom, jerry]
+
+for index, pet in enumerate(pets, start=1):
+    print(
+        f"{index}. {pet['name'].title()} is a {pet['kind']} living with {pet['owner'].title()}"
+    )
+
+# 1. Spike is a dog living with Dung
+# 2. Tom is a cat living with Khoa
+# 3. Jerry is a mouse living with Laurence
