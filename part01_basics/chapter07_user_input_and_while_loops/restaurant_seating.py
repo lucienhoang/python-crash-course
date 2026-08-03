9fdd67f213d2decd4968a11de74e571b44df99aa
lucienhoang <nosewater.station@gmail.com>
2026-08-03 10:30:58 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter07_user_input_and_while_loops/restaurant_seating.py
@@ -0,0 +1,11 @@
+number_of_people = input("How many people are there in your group? ")
+number_of_people = int(number_of_people)
+
+if number_of_people > 8:
+    print("\nYou guys have to wait for a table!")
+else:
+    print("\nYour table is ready!")
+
+# How many people are there in your group? 3
+
+# Your table is ready!
