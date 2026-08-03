f8ebfda30912ae1389054aaba5024e232bde5de9
lucienhoang <nosewater.station@gmail.com>
2026-08-03 09:48:22 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter07_user_input_and_while_loops/greeter.py
@@ -0,0 +1,12 @@
+# name = input("Please enter your name: ")
+# print(f"Hello {name}!")
+
+prompt = "If you tell us who you are, we can personalize the message you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print(f"Hello {name.title()}!")
+
+# If you tell us who you are, we can personalize the message you see.
+# What is your first name? luci
+# Hello Luci!
