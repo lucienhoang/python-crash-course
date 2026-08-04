3f2ec8afa3b8027450d59e3ee93c584e9e229bc0
lucienhoang <nosewater.station@gmail.com>
2026-08-04 10:59:02 +0700

    Added code files for entire book.

--- part01_basics/chapter07_user_input_and_while_loops/movie_tickets.py
+++ part01_basics/chapter07_user_input_and_while_loops/movie_tickets.py
@@ -15,3 +15,18 @@ while True:
         print("The ticket is 10$.")
     else:
         print("The ticket is 15$.")
+
+# Tell me your age, and i will tell you the cost of your movie ticket.
+# Enter 'quit' to end the program. 2
+# The ticket is free.
+
+# Tell me your age, and i will tell you the cost of your movie ticket.
+# Enter 'quit' to end the program. 5
+# The ticket is 10$.
+
+# Tell me your age, and i will tell you the cost of your movie ticket.
+# Enter 'quit' to end the program. 15
+# The ticket is 15$.
+
+# Tell me your age, and i will tell you the cost of your movie ticket.
+# Enter 'quit' to end the program. quit
