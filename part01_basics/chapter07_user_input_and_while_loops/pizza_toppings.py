54b0116b589153452b2c4d844852a1677d2c6171
lucienhoang <nosewater.station@gmail.com>
2026-08-04 10:28:34 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter07_user_input_and_while_loops/pizza_toppings.py
@@ -0,0 +1,31 @@
+prompt = "\nTell me the topping that you want to add."
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True
+
+# while active:
+#     message = input(prompt)
+
+#     if message == "quit":
+#         active = False
+#     else:
+#         print(f"You will add {message} to your pizza.")
+
+while True:
+    message = input(prompt)
+
+    if message == "quit":
+        break
+
+    print(f"You will add {message} to your pizza.")
+
+# Tell me the topping that you want to add.
+# Enter 'quit' to end the program. khoai lang
+# You will add khoai lang to your pizza.
+
+# Tell me the topping that you want to add.
+# Enter 'quit' to end the program. dua hau
+# You will add dua hau to your pizza.
+
+# Tell me the topping that you want to add.
+# Enter 'quit' to end the program. quit
