00c4a3bdbde5a845d8f160ceba8a1b1878433e44
lucienhoang <nosewater.station@gmail.com>
2026-08-05 16:45:18 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter08_functions/greeter.py
@@ -0,0 +1,10 @@
+def greet_user(username):
+    """Display a simple greetting"""  # docstring
+    print(f"Hello {username.title()}!")
+
+
+greet_user("luci")
+greet_user("bong bang")
+
+# Hello Luci!
+# Hello Bong Bang!
