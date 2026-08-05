474d7e10859eeb1a91f2a94a4f6ecc78c41fde5f
lucienhoang <nosewater.station@gmail.com>
2026-08-05 21:39:04 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter08_functions/8_1_message.py
@@ -0,0 +1,10 @@
+def display_message():
+    prompt = "I am doing my best to have a bright future!"
+    prompt += "\nWish me luck!"
+    print(prompt)
+
+
+display_message()
+
+# I am doing my best to have a bright future!
+# Wish me luck!
