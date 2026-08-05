2090c41ca95b60da24c963b2859a6c4bb1b7b55d
lucienhoang <nosewater.station@gmail.com>
2026-08-05 21:45:19 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter08_functions/8_2_favorite_book.py
@@ -0,0 +1,9 @@
+def favorite_book(title):
+    print(f"One of my favorite book is {title.title()}!")
+
+
+favorite_book("The God Father")
+favorite_book("mot doi nhu ke tim duong")
+
+# One of my favorite book is The God Father!
+# One of my favorite book is Mot Doi Nhu Ke Tim Duong!
