73089c9dbfe81507ccf4782ed615280722f48ea6
lucienhoang <nosewater.station@gmail.com>
2026-08-09 17:59:38 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter08_functions/8_9_magicians.py
@@ -0,0 +1,14 @@
+def show_magiccians(names):
+    """Print the names of the magicians"""
+    print("List of Magician's names:")
+    for name in names:
+        print(f"{name.title()}")
+
+
+magician_name = ["luci", "khoa", "dung"]
+show_magiccians(magician_name)
+
+# List of Magician's names:
+# Luci
+# Khoa
+# Dung
