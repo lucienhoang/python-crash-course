53d0748f63e7d76cf11c327024a4ddc57eecbd6b
lucienhoang <nosewater.station@gmail.com>
2026-08-15 17:12:28 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter09_classes/9_3_users.py
@@ -0,0 +1,59 @@
+class User:
+    """Model a User"""
+
+    def __init__(self, first_name, last_name, gender, field):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.gender = gender
+        self.field = field
+
+    def descibe_user(self):
+        print("\nUser information:")
+        print(f"First name: {self.first_name.title()}")
+        print(f"Last name: {self.last_name.title()}")
+        print(f"Gender: {self.gender}")
+        print(f"Field: {self.field}")
+
+    def greet_user(self):
+        print("---")
+        name = self.first_name.title() + " " + self.last_name.title()
+        print(f"Hello {name}!")
+
+
+user_1 = User("Khoa", "Hoang", "Male", "IT")
+user_2 = User("Dung", "Le", "Female", "Korean culture")
+user_3 = User("Laurence", "Huynh", "Male", "English")
+
+user_1.descibe_user()
+user_1.greet_user()
+
+user_2.descibe_user()
+user_2.greet_user()
+
+user_3.descibe_user()
+user_3.greet_user()
+
+
+# User information:
+# First name: Khoa
+# Last name: Hoang
+# Gender: Male
+# Field: IT
+# ---
+# Hello Khoa Hoang!
+
+# User information:
+# First name: Dung
+# Last name: Le
+# Gender: Female
+# Field: Korean culture
+# ---
+# Hello Dung Le!
+
+# User information:
+# First name: Laurence
+# Last name: Huynh
+# Gender: Male
+# Field: English
+# ---
+# Hello Laurence Huynh!
