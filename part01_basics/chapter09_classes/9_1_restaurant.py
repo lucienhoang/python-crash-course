67aee97a0520caf56f740ac76f78c6469dccedac
lucienhoang <nosewater.station@gmail.com>
2026-08-16 08:47:22 +0700

    Added code files for entire book.

--- part01_basics/chapter09_classes/9_1_restaurant.py
+++ part01_basics/chapter09_classes/9_1_restaurant.py
@@ -6,14 +6,14 @@ class Restaurant:
         self.cuisine_type = cuisine_type
 
     def describe_restaurant(self):
-        print(f"The name of restaurant is {self.restaurant_name.title()}.")
+        print(f"The restaurant's name is {self.restaurant_name.title()}.")
         print(f"{self.restaurant_name.title()} serves {self.cuisine_type} cuisine.")
 
     def open_restaurant(self):
         print("\tThe restaurant is open!")
 
 
-my_restaurant = Restaurant("Lucy Dinner", "Viet food")
+my_restaurant = Restaurant("Lucy Diner", "Vietnamese")
 
 print(my_restaurant.restaurant_name)
 print(my_restaurant.cuisine_type)
@@ -21,8 +21,8 @@ print(my_restaurant.cuisine_type)
 my_restaurant.describe_restaurant()
 my_restaurant.open_restaurant()
 
-# Lucy Dinner
-# Viet food
-# The name of restaurant is Lucy Dinner.
-# Lucy Dinner serves Viet food cuisine.
+# Lucy Diner
+# Vietnamese
+# The restaurant's name is Lucy Diner.
+# Lucy Diner serves Vietnamese cuisine.
 #         The restaurant is open!
--- part01_basics/chapter09_classes/9_4_number_served.py
+++ part01_basics/chapter09_classes/9_4_number_served.py
@@ -1,5 +1,5 @@
 class Restaurant:
-    """Model a restaurant"""
+    """Model a restaurant."""
 
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
@@ -7,7 +7,7 @@ class Restaurant:
         self.number_served = 0
 
     def describe_restaurant(self):
-        print(f"The name of restaurant is {self.restaurant_name.title()}.")
+        print(f"The restaurant's name is {self.restaurant_name.title()}.")
         print(f"{self.restaurant_name.title()} serves {self.cuisine_type} cuisine.")
 
     def set_number_served(self, value):
@@ -17,29 +17,29 @@ class Restaurant:
         self.number_served += value
 
 
-my_restaurant = Restaurant("Lucy Dinner", "Viet food")
+my_restaurant = Restaurant("Lucy Diner", "Vietnamese")
 
 my_restaurant.describe_restaurant()
-print(f"Number of customers have been served: {my_restaurant.number_served}")
+print(f"Number of customers served: {my_restaurant.number_served}")
 
 print("---")
 my_restaurant.number_served = 23
-print(f"Number of customers have been served: {my_restaurant.number_served}")
+print(f"Number of customers served: {my_restaurant.number_served}")
 
-# The name of restaurant is Lucy Dinner.
-# Lucy Dinner serves Viet food cuisine.
-# Number of customers have been served: 0
+# The restaurant's name is Lucy Diner.
+# Lucy Diner serves Vietnamese cuisine.
+# Number of customers served: 0
 # ---
-# Number of customers have been served: 23
+# Number of customers served: 23
 
 print("---")
 my_restaurant.set_number_served(43)
-print(f"Number of customers have been served: {my_restaurant.number_served}")
+print(f"Number of customers served: {my_restaurant.number_served}")
 
-# Number of customers have been served: 43
+# Number of customers served: 43
 
 print("---")
 my_restaurant.increment_number_served(20)
-print(f"Number of customers have been served: {my_restaurant.number_served}")
+print(f"Number of customers served: {my_restaurant.number_served}")
 
-# Number of customers have been served: 63
+# Number of customers served: 63
