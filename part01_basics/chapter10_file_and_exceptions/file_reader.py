2ee09efd937daa89fff4b6898563c79258040a42
lucienhoang <nosewater.station@gmail.com>
2026-08-17 11:54:04 +0700

    Added code files for entire book.

--- part01_basics/chapter10_file_and_exceptions/file_reader.py
+++ part01_basics/chapter10_file_and_exceptions/file_reader.py
@@ -1,3 +1,8 @@
 with open("part01_basics/chapter10_file_and_exceptions/pi_digits.txt") as file_object:
     contents = file_object.read()
     print(contents.rstrip())
+
+
+#  3.1415926535
+#    8979323846
+#    2643383279
