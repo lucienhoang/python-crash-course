aebbf1d133d0a83e7634cdc679961090cafd3204
lucienhoang <nosewater.station@gmail.com>
2026-08-17 16:31:44 +0700

    Added code files for entire book.

--- part01_basics/chapter10_file_and_exceptions/file_reader.py
+++ part01_basics/chapter10_file_and_exceptions/file_reader.py
@@ -1,4 +1,11 @@
-with open("part01_basics/chapter10_file_and_exceptions/pi_digits.txt") as file_object:
+# Relative file path
+file_path_1 = "part01_basics/chapter10_file_and_exceptions/pi_digits.txt"
+
+# Absolute file path
+file_path_2 = (
+    "D:/python-crash-course/part01_basics/chapter10_file_and_exceptions/pi_digits.txt"
+)
+with open(file_path_2) as file_object:
     contents = file_object.read()
     print(contents.rstrip())
 
