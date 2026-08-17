3edf4e5ad95e9b17161c0401781cfd7551142d73
lucienhoang <nosewater.station@gmail.com>
2026-08-17 09:16:07 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter09_classes/my_cars_2.py
@@ -0,0 +1,11 @@
+# Importing an entire                                                               Module
+import electric_car_3
+
+my_beetle = electric_car_3.Car("volkswagen", "beetle", 2016)
+print(my_beetle.get_descriptive_name())
+
+my_tesla = electric_car_3.ElectricCar("tesla", "roaster", 2016)
+print(my_tesla.get_descriptive_name())
+
+# 2016 Volkswagen Beetle
+# 2016 Tesla Roaster
