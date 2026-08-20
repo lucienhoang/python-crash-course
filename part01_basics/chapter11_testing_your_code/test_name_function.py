1da427a47836604d2c05f07b5df01c6ad745cbf7
lucienhoang <nosewater.station@gmail.com>
2026-08-20 15:18:05 +0700

    Added code files for entire book.

---
+++ part01_basics/chapter11_testing_your_code/test_name_function.py
@@ -0,0 +1,18 @@
+import unittest
+from name_function import get_formatted_name
+
+
+class NameTestCase(unittest.TestCase):
+    """Test for 'name_functions.py'."""
+
+    def test_first_last_name(self):
+        """Do names like 'Janis Joplin' work?"""
+        formatted_name = get_formatted_name("janis", "joplin")
+        self.assertEqual(formatted_name, "Janis Joplin")
+
+
+unittest.main()
+
+# .
+# ----------------------------------------------------------------------
+# Ran 1 test in 0.000s
