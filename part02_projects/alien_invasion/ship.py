2768414db6e1ab4bb9b78b0521513be1380c1d66
lucienhoang <nosewater.station@gmail.com>
2026-08-21 14:51:59 +0700

    Adjusting the Ship's Speed.

--- part02_projects/alien_invasion/ship.py
+++ part02_projects/alien_invasion/ship.py
@@ -2,9 +2,10 @@ import pygame
 
 
 class Ship:
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting positions."""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load(
@@ -19,16 +20,23 @@ class Ship:
         self.rect.centerx = self.screen_rect.centerx  #  The x-coordinate of the ship’s center match the centerx attribute of the screen’s rect.
         self.rect.bottom = self.screen_rect.bottom  # The y-coordinate of the ship’s bottom equal to the value of the screen rect’s bottom attribute.
 
+        # Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on the movement flag."""
+        # Update the ship's center value, not the rect.
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the ship at its current location."""
