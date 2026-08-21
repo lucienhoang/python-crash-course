198a16fc8b8fa0672294a49f45272c6ac560affd
lucienhoang <nosewater.station@gmail.com>
2026-08-21 13:21:35 +0700

    Refactor: the game_functions Module.

--- part02_projects/alien_invasion/alien_invasion.py
+++ part02_projects/alien_invasion/alien_invasion.py
@@ -1,9 +1,9 @@
 # .\.venv\Scripts\Activate.ps1
-import sys  # Use the sys module to exit the game.
 
 import pygame  # The pygame module contains the functionality to make a game.
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -21,16 +21,9 @@ def run_game():
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through loop.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.check_events()
+        # Update screen.
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
