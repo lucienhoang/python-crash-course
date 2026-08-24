9fe3b98f0c670708dee6759d4eea16bc0e43c6e3
lucienhoang <nosewater.station@gmail.com>
2026-08-24 09:32:03 +0700

    hiding the Mouse Cursor.

--- part02_projects/alien_invasion/game_functions.py
+++ part02_projects/alien_invasion/game_functions.py
@@ -79,6 +79,9 @@ def check_play_button(
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     """Start a new game when the player clicks Play."""
     if button_clicked and not stats.game_active:
+        # Hide the mouse cursor.
+        pygame.mouse.set_visible(False)
+
         # Reset the game statistics.
         stats.reset_stats()
         stats.game_active = True
@@ -207,6 +210,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         sleep(0.5)
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
