7b239b5fc0745186ddb267ff288c9861308cd93a
lucienhoang <nosewater.station@gmail.com>
2026-08-24 13:52:47 +0700

    Increasing Point Values.

--- part02_projects/alien_invasion/settings.py
+++ part02_projects/alien_invasion/settings.py
@@ -26,6 +26,9 @@ class Settings:
         # How quickly the game speeds up
         self.speedup_scale = 1.1
 
+        # How quicly the alien point values increse
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -39,7 +42,9 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase spped settings."""
+        """Increase spped settingsb and alien point value."""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
