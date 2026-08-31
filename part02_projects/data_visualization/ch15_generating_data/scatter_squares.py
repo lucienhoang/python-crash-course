a13cbc4d5960fb847f9b341f0acecc0004f6d7d3
lucienhoang <nosewater.station@gmail.com>
2026-08-31 20:12:03 +0700

    Plotting a Series of Points with scatter().

--- part02_projects/data_visualization/ch15_generating_data/scatter_squares.py
+++ part02_projects/data_visualization/ch15_generating_data/scatter_squares.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
-plt.plot(input_values, squares, lw=5)
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.scatter(x_values, y_values, s=100)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
@@ -10,6 +11,6 @@ plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
-plt.tick_params(axis="both", labelsize=14)
+plt.tick_params(axis="both", which="major", labelsize=14)
 
 plt.show()
