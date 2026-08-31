56219bd160b0c84c3f7668574113dd00eaeecce0
lucienhoang <nosewater.station@gmail.com>
2026-08-31 22:33:37 +0700

    Saving the Plot Automatically.

--- part02_projects/data_visualization/ch15_generating_data/scatter_squares.py
+++ part02_projects/data_visualization/ch15_generating_data/scatter_squares.py
@@ -16,4 +16,7 @@ plt.tick_params(axis="both", which="major", labelsize=14)
 # Set the range for each axis.
 plt.axis([0, 1100, 0, 1100000])
 
+# Saving the Plot Automatically.
+# plt.savefig("squares_plot.png", bbox_inches="tight")
+
 plt.show()
