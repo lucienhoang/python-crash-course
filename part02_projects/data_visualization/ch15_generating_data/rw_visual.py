79fac81bf1eadc59d3612b8aa10997cc32d8af66
lucienhoang <nosewater.station@gmail.com>
2026-09-01 12:08:33 +0700

    Adding Plot Points.

--- part02_projects/data_visualization/ch15_generating_data/rw_visual.py
+++ part02_projects/data_visualization/ch15_generating_data/rw_visual.py
@@ -7,9 +7,9 @@ while True:
     rw = RandomWalk()
     rw.fill_walk()
 
+    # Plot the points, and show the plot.
     point_numbers = list(range(rw.num_points))
-
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, edgecolors="none", s=15)
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, edgecolors="none", s=1)
 
     # Emphasize the first and last points.
     plt.scatter(0, 0, c="orange", edgecolors="none", s=100)
