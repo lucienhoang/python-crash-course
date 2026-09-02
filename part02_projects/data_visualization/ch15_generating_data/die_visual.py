03ce98336094ab049808ad34996ccfd02dbe8777
lucienhoang <nosewater.station@gmail.com>
2026-09-02 11:00:37 +0700

    Practicing with Both Libraries.

--- part02_projects/data_visualization/ch15_generating_data/die_visual.py
+++ part02_projects/data_visualization/ch15_generating_data/die_visual.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pygal
 from die import Die
 
@@ -20,3 +21,31 @@ hist.y_title = "Frequency of Result."
 
 hist.add("D6", frequencies)
 hist.render_to_file("die_visual.svg")
+
+# Visualize the results with Matplotlib.
+plt.figure(figsize=(10, 6))
+
+# Prepare x label.
+x_labels = [x for x in range(1, die.num_sides + 1)]
+
+# Draw the plot.
+plt.plot(x_labels, frequencies, marker="o", lw=2, color="steelblue")
+
+# Set chart title and label axes.
+plt.title("Results of rolling one D6 1000 times.", fontsize=24)
+plt.xlabel("Result", fontsize=14)
+plt.ylabel("Frequency of Result", fontsize=14)
+
+# Set size of tick labels.
+plt.tick_params(axis="both", labelsize=14)
+
+# Set gridline.
+plt.grid(True, linestyle="--", alpha=0.4)
+
+# Set tick.
+plt.xticks(x_labels)
+
+# Set limit for y axes.
+plt.ylim(0, max(frequencies) + 10)
+
+plt.show()
