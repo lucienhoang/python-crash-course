f4de2ab0d35d1d0850962063104c9e9d56a61c0a
lucienhoang <nosewater.station@gmail.com>
2026-09-02 21:47:30 +0700

    Plotting Dates.

--- part02_projects/data_visualization/ch16_downloading_data/highs_lows.py
+++ part02_projects/data_visualization/ch16_downloading_data/highs_lows.py
@@ -1,16 +1,21 @@
 import csv
+from datetime import datetime
 
 from matplotlib import pyplot as plt
 
-# Get high temperatures from file.
+# Get dates and high temperatures from file.
 filename = "files/sitka_weather_2021.csv"
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    highs = []
+    dates, highs = [], []
+
     for row in reader:
         if row[4] != "":
+            current_date = datetime.strptime(row[2], "%Y-%m-%d")  # noqa: DTZ007
+            dates.append(current_date)
+
             high = int(row[4])
             highs.append(high)
 
@@ -18,11 +23,12 @@ with open(filename) as f:
 
 # Plot data.
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c="red")
+plt.plot(dates, highs, c="red")
 
 # Format plot.
-plt.title("Daily high temprratures, 2021.")
+plt.title("Daily high temperatures, 2021.")
 plt.xlabel("", fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis="both", which="major", labelsize=16)
 
