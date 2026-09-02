717588145be968e99bc8a386f1fa5403a16e74bb
lucienhoang <nosewater.station@gmail.com>
2026-09-02 09:46:06 +0700

    Automatic Labels.

--- part02_projects/data_visualization/ch15_generating_data/dice_visual.py
+++ part02_projects/data_visualization/ch15_generating_data/dice_visual.py
@@ -6,24 +6,18 @@ die_1 = Die()
 die_2 = Die()
 
 # Make some rolls, and store results in a list.
-results = []
-
-for roll_num in range(1000):
-    result = die_1.roll() + die_2.roll()
-    results.append(result)
+results = [die_1.roll() + die_2.roll() for roll_num in range(1000)]
 
 # Analyze the results.
-frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
-for value in range(2, max_result + 1):
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in range(2, max_result + 1)]
+
 
 # Visualize the results.
 hist = pygal.Bar()
 
 hist.title = "Results of rolling two D6 dice 1000 times."
-hist.x_labels = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+hist.x_labels = [x for x in range(2, max_result)]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result."
 
--- part02_projects/data_visualization/ch15_generating_data/die_visual.py
+++ part02_projects/data_visualization/ch15_generating_data/die_visual.py
@@ -5,23 +5,16 @@ from die import Die
 die = Die()
 
 # Make some rolls, and store results in a list.
-results = []
-
-for roll_num in range(1000):
-    result = die.roll()
-    results.append(result)
+results = [die.roll() for roll_num in range(1000)]
 
 # Analyze the results.
-frequencies = []
-for value in range(1, die.num_sides + 1):
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in range(1, die.num_sides + 1)]
 
 # Visualize the results.
 hist = pygal.Bar()
 
 hist.title = "Results of rolling one D6 1000 times."
-hist.x_labels = ["1", "2", "3", "4", "5", "6"]
+hist.x_labels = [x for x in range(1, die.num_sides + 1)]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result."
 
