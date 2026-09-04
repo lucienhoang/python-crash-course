1c2b51791108a9ad656f021121d7f7f04efa878d
lucienhoang <nosewater.station@gmail.com>
2026-09-04 15:58:50 +0700

    Grouping Countries by Populations.

--- part02_projects/data_visualization/ch16_downloading_data/world_population.py
+++ part02_projects/data_visualization/ch16_downloading_data/world_population.py
@@ -18,8 +18,23 @@ for pop_dict in pop_data:
         if code:
             cc_population[code] = population
 
+# Group the countries into 3 population levels.
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_population.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+
+# See how many countries are in each level.
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
 wm = pygal_maps_world.maps.World()
 wm.title = "World Population in 2010, by Country."
-wm.add("2010", cc_population)
+wm.add("0-10m", cc_pops_1)
+wm.add("10m-1bn", cc_pops_2)
+wm.add(">1bn", cc_pops_3)
 
 wm.render_to_file("files/world_population.svg")
