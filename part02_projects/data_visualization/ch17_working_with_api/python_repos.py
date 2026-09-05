48d367234cfd9b49c90a8756436252700dcc6eb9
lucienhoang <nosewater.station@gmail.com>
2026-09-05 11:07:41 +0700

    Visualizing Repositories Using Pygal.

--- part02_projects/data_visualization/ch17_working_with_api/python_repos.py
+++ part02_projects/data_visualization/ch17_working_with_api/python_repos.py
@@ -1,4 +1,7 @@
+import pygal
 import requests
+from pygal.style import LightColorizedStyle as LCS
+from pygal.style import LightenStyle as LS
 
 # Make an API call and store the response.
 url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
@@ -19,15 +22,30 @@ repo_dicts = response_dict["items"]
 print("Repositories returned:", len(repo_dicts))
 
 # Examine the first repository.
-repo_dict = repo_dicts[0]
+# repo_dict = repo_dicts[0]
+
 # print("\nKey:", len(repo_dict))
 # for key in sorted(repo_dict.keys()):
 #     print(key)
-print("\nSelected information about first repository:")
-print("Name:", repo_dict["name"])
-print("Owner:", repo_dict["owner"]["login"])
-print("Starts:", repo_dict["stargazers_count"])
-print("Repository:", repo_dict["html_url"])
-print("Created:", repo_dict["created_at"])
-print("Updated:", repo_dict["updated_at"])
-print("Description:", repo_dict["description"])
+
+# print("\nSelected information about each repository:")
+# for repo_dict in repo_dicts:
+#     print("\nName:", repo_dict["name"])
+#     print("Owner:", repo_dict["owner"]["login"])
+#     print("Starts:", repo_dict["stargazers_count"])
+#     print("Repository:", repo_dict["html_url"])
+#     print("Description:", repo_dict["description"])
+
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict["name"])
+    stars.append(repo_dict["stargazers_count"])
+
+# Make visualization.
+my_style = LS("#333366", base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = "Most-Starred Python Projects on Github"
+chart.x_labels = names
+
+chart.add("", stars)
+chart.render_to_file("files/python_repos.svg")
